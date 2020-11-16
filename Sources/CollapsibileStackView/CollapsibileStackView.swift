e214ce74478ebab6509d84a83b0c091c22c870d4
Amandeep <uncommon.aman@gmail.com>
2020-11-16 23:29:45 +0530

    Convert stack view into swift package

--- Sources/CollapsibileStackView/CollapsibileStackView.swift
+++ Sources/CollapsibileStackView/CollapsibileStackView.swift
@@ -1,3 +1,174 @@
-struct CollapsibileStackView {
-    var text = "Hello, World!"
+//
+//  CollapsibileStackView.swift
+//  CollapsibleStackView
+//
+//  Created by Amandeep on 16/11/20.
+//  Copyright © 2020 Amandeep. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol CollapsibleStackViewDatasource: class {
+    func cardStack(_ collapsibleStackView: CollapsibleStackView, viewForIndex index: Int) -> UIView
+    func cardStack(_ collapsibleStackView: CollapsibleStackView, headerForIndex index: Int) -> UIView
+    func numberOfItems(in collapsibleStackView: CollapsibleStackView) -> Int
+    func cardStack(_ collapsibleStackView: CollapsibleStackView, footerForIndex index: Int) -> UIView?
+}
+
+public protocol CollapsibleStackViewDelegate: class {
+    func cardStack(_ collapsibleStackView: CollapsibleStackView, didSelectItemAt index: Int)
+    func cardStack(_ collapsibleStackView: CollapsibleStackView, didSelectFooterAt index: Int)
+}
+
+
+public class CollapsibleStackView: UIStackView {
+    //MARK:- Props
+    private var index: Int = 0
+    private var footer: UIView?
+    private var numberOfItems = 0
+    private let collapseAnimationSync = DispatchGroup()
+    
+    lazy private var bodyStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        return stackView
+    }()
+    //MARK:- Delegates
+    public weak var datasource: CollapsibleStackViewDatasource? {
+        didSet {
+            load()
+        }
+    }
+    public weak var delegate: CollapsibleStackViewDelegate?
+    
+    //MARK:- Init
+    public init() {
+        super.init(frame: .zero)
+        self.axis = .vertical
+        self.addArrangedSubview(bodyStackView)
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+//MARK:- Data loadeer
+extension CollapsibleStackView {
+    private func load() {
+        index = 0
+        guard let datasource = datasource else { return }
+        numberOfItems = datasource.numberOfItems(in: self)
+        
+        let header = datasource.cardStack(self, headerForIndex: index)
+        let body = datasource.cardStack(self, viewForIndex: index)
+        
+        
+        let collapsibleStackView = CollapsibleStackViewItem([header,body], makeHeightZero: false, delegate: self)
+        
+        addTapGesture(view: collapsibleStackView)
+        self.bodyStackView.addArrangedSubview(collapsibleStackView)
+        
+        guard let footer = datasource.cardStack(self, footerForIndex: index) else { return }
+        self.footer = footer
+        self.addArrangedSubview(footer)
+        
+    }
+    
+    public func showNextCard() {
+        if (index + 1) >=  numberOfItems {
+            return
+        }
+        
+        index += 1
+        guard let datasource = datasource else { return }
+        let header = datasource.cardStack(self, headerForIndex: index)
+        let body = datasource.cardStack(self, viewForIndex: index)
+        self.footer?.removeFromSuperview()
+        
+        let collapsibleStackView = CollapsibleStackViewItem([header,body], delegate: self)
+        addTapGesture(view: collapsibleStackView)
+        self.bodyStackView.addArrangedSubview(collapsibleStackView)
+        
+        
+        collapsibleStackView.state = .expanded
+        let prev = self.bodyStackView.arrangedSubviews[index-1] as! CollapsibleStackViewItem
+        prev.state = .partialCollapsed
+        
+        self.footer = datasource.cardStack(self, footerForIndex: index)
+        
+    }
+    
+    public func reloadHeader(at index: Int) {
+        guard let newHeader = datasource?.cardStack(self, headerForIndex: index) else { return }
+        let container = (self.bodyStackView.arrangedSubviews[index] as! CollapsibleStackViewItem)
+        let view = container.arrangedSubviews[0]
+        view.removeFromSuperview()
+        container.insertArrangedSubview(newHeader, at: 0)
+    }
+    
+    public func reloadBody(at index: Int) {
+        guard let newBody = datasource?.cardStack(self, viewForIndex: index) else { return}
+        let container = (self.bodyStackView.arrangedSubviews[index] as! CollapsibleStackViewItem)
+        let body = container.arrangedSubviews[1]
+        body.removeFromSuperview()
+        container.insertArrangedSubview(newBody, at: 1)
+    }
+    
+}
+
+//MARK:- Touch events
+extension CollapsibleStackView {
+    private func addTapGesture(view: UIView) {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapView))
+        view.isUserInteractionEnabled = true
+        view.tag = self.index
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func didTapView(sender:UITapGestureRecognizer) {
+        guard let view = sender.view, view.tag != self.index else { return }
+        self.footer?.removeFromSuperview()
+        index = view.tag
+        
+        self.footer = datasource?.cardStack(self, footerForIndex: index)
+        
+        
+        for i in stride(from: self.bodyStackView.arrangedSubviews.count - 1, to: index, by: -1) {
+            let viewToCollapse = self.bodyStackView.arrangedSubviews[i] as! CollapsibleStackViewItem
+            self.collapseAnimationSync.enter()
+            viewToCollapse.state = .collapsed
+        }
+        
+        
+        let viewToExpand = self.bodyStackView.arrangedSubviews[self.index] as! CollapsibleStackViewItem
+        viewToExpand.state = .partialExpanded
+        
+    }
+}
+
+//MARK:- Animation events
+extension CollapsibleStackView: AnimationDelegate {
+    func didFinishExpandAnimation() {
+        if let footer = footer {
+            self.addArrangedSubview(footer)
+        }
+        delegate?.cardStack(self, didSelectFooterAt: index-1)
+    }
+    
+    func didFinishCollapseAnimation() {
+        collapseAnimationSync.leave()
+        
+        collapseAnimationSync.notify(queue: .main) {
+            if let footer = self.footer {
+                self.addArrangedSubview(footer)
+            }
+            self.delegate?.cardStack(self, didSelectItemAt: self.index)
+        }
+    }
+    
 }
---
+++ Sources/CollapsibileStackView/CollapsibileStackViewItem.swift
@@ -0,0 +1,127 @@
+//
+//  CollapsibileStackViewItem.swift
+//  CollapsibleStackView
+//
+//  Created by Amandeep on 16/11/20.
+//  Copyright © 2020 Amandeep. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol AnimationDelegate:class {
+    func didFinishExpandAnimation()
+    func didFinishCollapseAnimation()
+}
+
+public enum CollapsibleStackViewItemState {
+    case collapsed
+    case expanded
+    case partialCollapsed
+    case partialExpanded
+    
+    fileprivate var isZeroHeightConstraintActive: Bool {
+        switch self {
+        case .collapsed,.partialCollapsed:
+            return true
+        case .expanded,.partialExpanded:
+            return false
+        }
+    }
+}
+
+
+public class CollapsibleStackViewItem: UIStackView {
+    weak var delegate: AnimationDelegate?
+    public var state: CollapsibleStackViewItemState = .collapsed {
+        didSet {
+            switch state {
+            case .expanded:
+                self.expand()
+            case .partialCollapsed:
+                self.partiallyCollapse()
+            case .collapsed:
+                self.collapse()
+            case .partialExpanded:
+                self.partiallyExpand()
+            }
+        }
+    }
+    
+    private var zeroHeightConstraint: NSLayoutConstraint?
+    private var bodyZeroHeightConstraint: NSLayoutConstraint?
+    
+    init(_ views: [UIView], makeHeightZero: Bool = true, delegate: AnimationDelegate?) {
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.delegate = delegate
+        self.axis = .vertical
+        if makeHeightZero {
+            self.addZeroHeightConstraint()
+        }
+        _ = views.map { addArrangedSubview($0) }
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func addZeroHeightConstraint() {
+        zeroHeightConstraint = self.heightAnchor.constraint(equalToConstant: 0)
+        zeroHeightConstraint?.isActive = true
+        
+    }
+        
+    private func expand() {
+        self.superview?.superview?.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.zeroHeightConstraint?.isActive = self.state.isZeroHeightConstraintActive
+            self.superview?.superview?.layoutIfNeeded()
+        }) { (_) in
+            self.delegate?.didFinishExpandAnimation()
+        }
+    }
+    
+    private func partiallyCollapse() {
+        guard let body = self.arrangedSubviews.last else { return }
+        self.bodyZeroHeightConstraint = body.heightAnchor.constraint(equalToConstant: 0)
+    
+        self.superview?.superview?.layoutIfNeeded()
+        body.alpha = 1
+        UIView.animate(withDuration: 1, animations: {
+            body.alpha = 0
+              self.bodyZeroHeightConstraint?.isActive = self.state.isZeroHeightConstraintActive
+                      self.superview?.superview?.layoutIfNeeded()
+        }) { (_) in
+             
+        }
+    }
+    
+    private func collapse() {
+        self.alpha = 1
+        self.superview?.superview?.layoutIfNeeded()
+        self.bodyZeroHeightConstraint?.isActive = !self.state.isZeroHeightConstraintActive
+        UIView.animate(withDuration: 1, animations: {
+             self.alpha = 0
+            self.zeroHeightConstraint?.isActive = self.state.isZeroHeightConstraintActive
+            self.superview?.superview?.layoutIfNeeded()
+        }) { (_) in
+            self.removeFromSuperview()
+            self.delegate?.didFinishCollapseAnimation()
+        }
+    }
+    
+    private func partiallyExpand() {
+        guard let body = self.arrangedSubviews.last else { return }
+        body.alpha = 0
+        self.superview?.superview?.layoutIfNeeded()
+        UIView.animate(withDuration: 1) {
+            body.alpha = 1
+            self.bodyZeroHeightConstraint?.isActive = false
+            self.superview?.superview?.layoutIfNeeded()
+        }
+    }
+
+}
